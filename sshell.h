169471d118f640d52f8a234b018ec3fb6d5bdb0d
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-05-24 18:41:13 -0700

    first task

--- sshell.h
+++ sshell.h
@@ -5,10 +5,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h> 
+#include <sys/wait.h>
 
 int _islower(int c);
 void my_command(char **argv, char **envir);
-
 
 #endif
