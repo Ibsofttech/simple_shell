93ce34f860f05262f2718f39e8dd7957f66cbb41
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-05-26 03:00:28 -0700

    first task

--- my_command.c
+++ my_command.c
@@ -9,29 +9,30 @@
 
 void my_command(char **argv, char **envir)
 {
-	char *my_string = NULL;
-	int i, status;
+	char *my_string = NULL; /*msg = "cisfun$ "*/
+	int i = 0, status;
 	size_t n = 0;
 	ssize_t num_char;
 	char *args[] = {NULL, NULL};
+	bool from_pipe = false;
 	pid_t child_pid;
 
-	while (1)
+	while (1 && !from_pipe)
 	{
-		if (isatty(STDIN_FILENO))
-			printf("cisfun$ ");
-
+		if (isatty(STDIN_FILENO) == 0)
+			from_pipe = true;
+		/*write(1, msg, 8);*/
+		printf("cisfun$ ");
 		num_char = getline(&my_string, &n, stdin);
 		if (num_char == -1)
 		{
 			free(my_string);
 			exit(EXIT_FAILURE);
 		}
-		i = 0;
 		while (my_string[i])
 		{
 			if (my_string[i] == '\n')
-				my_string[i] = 0;
+				my_string[i] = '\0';
 			i++;
 		}
 		args[0] = my_string;
@@ -43,7 +44,7 @@ void my_command(char **argv, char **envir)
 		}
 		if (child_pid == 0)
 		{
-			if (execve(args[0], args, envir) == -1)
+			if (execve(args[0], argv, envir) == -1)
 				printf("%s: No such file or directory\n", argv[0]);
 		}
 		else
--- sshell.h
+++ sshell.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 int _islower(int c);
 void my_command(char **argv, char **envir);
