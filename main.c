6aba2734480ceb5242c4932f63bbe764ac426140
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-07-28 19:02:06 +0000

    updated

--- main.c
+++ main.c
@@ -11,33 +11,32 @@
 
 int main(int argc __attribute__((unused)), char **argv, char **env)
 {
-    char *strings = NULL;
-    size_t nums = 0;
-    int i;
+	char *strings = NULL;
+	size_t nums = 0;
+	int i;
 
-    while (true)
-    {
-        printf("msh$ ");
+	while (true)
+	{
+		printf("msh$ ");
+		if (getline(&strings, &nums, stdin) == -1)
+		{
+			perror("Exiting shell");
+			break;
+		}
 
-        if (getline(&strings, &nums, stdin) == -1)
-        {
-            perror("Exiting shell");
-            break;
-        }
+		i = 0;
+		while (strings[i])
+		{
+			if (strings[i] == '\n')
+				strings[i] = '\0';
+			i++;
+		}
+		argv[0] = strings;
+		if (argv[0] == NULL)
+			continue;
+		execmc(argv, env);
+	}
 
-        i = 0;
-        while (strings[i])
-        {
-            if (strings[i] == '\n')
-                strings[i] = '\0';
-            i++;
-        }
-        argv[0] = strings;
-        if (argv[0] == NULL)
-            continue;
-        execmc(argv, env);
-    }
-
-    free(strings);
-    return 0;
+	free(strings);
+	return (0);
 }
