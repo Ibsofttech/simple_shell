145ffd357ce4aee9601c22e3d7df33f9b925346f
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-08-23 10:55:15 +0000

    corrrection

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 int main(int argc, char **argv)
 {
 	char *my_command = NULL;
+	char *args[2];
 	size_t num = 0;
 	ssize_t get_return;
 
@@ -17,25 +18,25 @@ int main(int argc, char **argv)
 	{
 		while (true)
 		{
+			_write("mshell$ ", STDOUT_FILENO);
+			get_return = getline(&my_command, &num, stdin);
+			if (get_return == -1)
 			{
-				_write("mshell$ ", STDOUT_FILENO);
-				get_return = getline(&my_command, &num, stdin);
-				if (get_return == -1)
-				{
-					free(my_command);
-					return (-1);
-				}
-				if (my_command[get_return - 1] == '\n')
-					my_command[get_return - 1] = '\0';
-
-				argv[0] = my_command;
-				execmc(argv);
+				free(my_command);
+				return (-1);
 			}
+			if (my_command[get_return - 1] == '\n')
+				my_command[get_return - 1] = '\0';
+
+			args[0] = my_command;
+			args[1] = NULL;
+			execmc(args);
 		}
 		free(my_command);
 		return (0);
 	}
-	else
+	else if (argc == 2)
 		return (non_interactive_mode(argv[1]));
-	return (1);
+	else
+		return (1);
 }
